cdc1421e02b0318e34aead3943a608b3e1a45d5b
Muhammadi2 <mshislam@gmail.com>
2021-10-21 23:45:37 -0400

    Added Logo

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
+import {ReactComponent as Logo} from '../images/logo.svg';
 
 const navbarstyle = {
-  backgroundColor: "lightblue",
+  backgroundColor: "#eeeeee",
 };
 
 const Header = (props) => {
@@ -10,7 +11,8 @@ const Header = (props) => {
   return (
     <Navbar style={navbarstyle} variant="light">
       <Container>
-        <Navbar.Brand href="/">{title}</Navbar.Brand>
+        {/* <Navbar.Brand href="/">{title}</Navbar.Brand> */}
+        <Logo style={{maxWidth: '12rem', maxHeight: '4rem'}}/>
       </Container>
     </Navbar>
   );
